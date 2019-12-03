b77321f77e3bcf6c66d706ad7c73da846ac4331d
seraph <seraphinush@gmail.com>
2019-12-04 08:48:14 +0900

    1.01.06

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const ZONE_RKHM = 9935;
+
 class rk9hm_guide_lite {
 
   constructor(mod) {
@@ -12,31 +14,29 @@ class rk9hm_guide_lite {
 
     this.enable = this.s.enable;
 
-    this.chatChannel = 0;
-    this.curZone = 0;
+    this.chat_ch = 0;
     this.loaded = false;
-    this.mechStrings = this.m.region === 'kr' ? ['근', '원', '터', 'ㅇ'] : ['out', 'in', 'wave', 'o'];
-    this.messageA = this.mechStrings[3];
-    this.messageB = this.mechStrings[3];
-    this.myZone = 0;
-    this.prevMechFirst = true;
-    this.thirdBoss = false;
+    this.mech_str = this.m.region === 'kr' ? ['근', '원', '터', 'ㅇ'] : ['out', 'in', 'wave', 'o'];
+    this.msg_a = this.msg_b = this.mech_str[3];
+    this.cur_zone = 0;
+    this.prev_mech_first = true;
+    this.third_boss = false;
 
     // command
     this.c.add('rk', {
       '$none': () => {
         this.enable = !this.enable;
-        this.enable && this.myZone === 9935 ? this.load() : this.unload();
+        this.enable && this.cur_zone === ZONE_RKHM ? this.load() : this.unload();
         this.send(`${this.enable ? 'En' : 'Dis'}abled`);
       },
       'party': () => { // 1
-        this.chatChannel !== 1 ? this.chatChannel = 1 : this.chatChannel = 0;
-        this.send(`Sending mechanics order to party chat ${this.chatChannel === 0 ? 'en' : 'dis'}abled.`);
+        this.chat_ch !== 1 ? this.chat_ch = 1 : this.chat_ch = 0;
+        this.send(`Sending mechanics order to party chat ${this.chat_ch !== 0 ? 'en' : 'dis'}abled.`);
       },
       'notice': () => { // 21
-        this.chatChannel !== 21 ? this.chatChannel = 21 : this.chatChannel = 0;
-        this.send(`Sending mechanics order to notice chat ${this.chatChannel === 0 ? 'en' : 'dis'}abled.`);
-        if (this.chatChannel === 21)
+        this.chat_ch !== 21 ? this.chat_ch = 21 : this.chat_ch = 0;
+        this.send(`Sending mechanics order to notice chat ${this.chat_ch !== 0 ? 'en' : 'dis'}abled.`);
+        if (this.chat_ch === 21)
           this.send(`Please make sure you are the party leader.`);
       },
       '$default': () => this.send(`Invalid argument. usage : rk [party|notice]`)
@@ -44,17 +44,17 @@ class rk9hm_guide_lite {
 
     // game state
     this.g.me.on('change_zone', (zone) => {
-      this.myZone = zone;
-      if (this.enable && !this.loaded && zone === 9935) {
+      this.cur_zone = zone;
+      if (this.enable && !this.loaded && zone === ZONE_RKHM) {
         this.load();
         this.loaded = true;
       }
-      else if (this.enable && this.loaded && zone !== 9935) {
+      else if (this.enable && this.loaded && zone !== ZONE_RKHM) {
         this.unload();
         this.loaded = false;
-        this.messageA = this.mechStrings[3];
-        this.messageB = this.mechStrings[3];
-        this.prevMechFirst = true;
+        this.msg_a = this.mech_str[3];
+        this.msg_b = this.mech_str[3];
+        this.prev_mech_first = true;
         this.send(`Leaving RK-9 Kennel (hard). unloaded guide module.`);
       }
     });
@@ -68,9 +68,9 @@ class rk9hm_guide_lite {
 
   // helper
   to_chat(msg) {
-    if (this.chatChannel) {
+    if (this.chat_ch !== 0) {
       this.m.send('C_CHAT', 1, {
-        channel: this.chatChannel,
+        channel: this.chat_ch,
         message: msg
       });
     }
@@ -81,10 +81,7 @@ class rk9hm_guide_lite {
 
   mech_order(delay) {
     this.m.setTimeout(() => {
-      if (this.prevMechFirst)
-        this.to_chat(`${this.messageA} ${this.messageB}`);
-      else
-        this.to_chat(`${this.messageB} ${this.messageA}`);
+      this.prev_mech_first ? this.to_chat(`${this.msg_a} ${this.msg_b}`) : this.to_chat(`${this.msg_b} ${this.msg_a}`);
     }, delay);
   }
 
@@ -95,31 +92,31 @@ class rk9hm_guide_lite {
 
   load() {
     this.hook('S_BOSS_GAGE_INFO', 3, (e) => {
-      this.thirdBoss = e.templateId === 3000;
+      this.third_boss = e.templateId === 3000;
     });
 
     this.hook('S_DUNGEON_EVENT_MESSAGE', 2, (e) => {
-      if (this.thirdBoss) {
+      if (this.third_boss) {
         let id = parseInt(e.message.replace('@dungeon:', ''));
         switch (id) {
           case 9935302:
-            this.messageA = this.mechStrings[0];
+            this.msg_a = this.mech_str[0];
             this.mech_order(2000);
             break;
           case 9935303:
-            this.messageA = this.mechStrings[1];
+            this.msg_a = this.mech_str[1];
             this.mech_order(2000);
             break;
           case 9935304:
-            this.messageA = this.mechStrings[2];
+            this.msg_a = this.mech_str[2];
             this.mech_order(2000);
             break;
           case 9935311:
-            this.prevMechFirst = true;
+            this.prev_mech_first = true;
             this.mech_order(2000);
             break;
           case 9935312:
-            this.prevMechFirst = false;
+            this.prev_mech_first = false;
             this.mech_order(2000);
             break;
         }
@@ -127,13 +124,13 @@ class rk9hm_guide_lite {
     });
 
     this.hook('S_QUEST_BALLOON', 1, (e) => {
-      if (this.thirdBoss) { // relevant IDs range from 935301 to 935303
+      if (this.third_boss) { // relevant IDs range from 935301 to 935303
         let index = parseInt(e.message.replace('@monsterBehavior:', '')) - 935301;
         if (0 <= index && index < 3) {
-          this.messageB = this.mechStrings[index];
+          this.msg_b = this.mech_str[index];
           this.m.setTimeout(() => {
-            this.messageA = this.messageB;
-            this.messageB = this.mechStrings[3];
+            this.msg_a = this.msg_b;
+            this.msg_b = this.mech_str[3];
             this.mech_order(6000);
           }, 8000);
         }
@@ -159,14 +156,13 @@ class rk9hm_guide_lite {
 
   // reload
   saveState() {
-    return this.myZone;
+    return this.cur_zone;
   }
 
   loadState(state) {
-    if (this.enable && !this.loaded && state === 9935) {
+    if (this.enable && !this.loaded && state === ZONE_RKHM) {
+      this.cur_zone = state;
       this.load();
-      this.loaded = true;
-      this.curZone = state;
     }
   }
 
